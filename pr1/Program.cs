80f7898ae2a1bdb9993294cf00fa757de333b4cf
pr-24.106-vp <pr-24.106-vp@115-02>
2025-12-17 15:00:19 +0700

    Добавьте файлы проекта.

---
+++ pr1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = 'X';
+            object boxed = symbol; // Boxing
+            char unboxed = (char)boxed; // Unboxing
+
+            var F = 3.14f;
+            var A = "null";
+            Console.WriteLine($"var number: {F}, Type:{ F.GetType()}");
+            Console.WriteLine($"var string: {A}, Type:{ A.GetType()}");
+
+            DateTime now = DateTime.Now;
+            string fromInt1 = now.ToString();
+            string fromInt2 = Convert.ToString(now);
+
+            Type intType = typeof(int);
+            Type stringType = typeof(string);
+            Console.WriteLine($"Type of int: {intType.Name}, Namespace:{ intType.Namespace}");
+
+        }
+    }
+}
